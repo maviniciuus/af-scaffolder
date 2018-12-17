19775675588f430f6a577b27f9cde867659159e4
Marcus Mendes <maviniciuus@gmail.com>
2018-12-17 08:47:01 -0200

    [UPD] - I added the structure to receive the new version, 2.0. With the responsibility of the manipulation of the strings, in the native lib helpers. I have transferred the responsibility of defining formatting for users.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 var _get = require("lodash").get;
-var scaffolder = require("./helpers/scaffolder");
+var scaffolder = require("./scaffolder");
 
 var argv = require("minimist")(process.argv.slice(2));
+
 //s = schemaPath - n or m = are the same, name of new module
-scaffolder.magic(_get(argv, "s"), _get(argv, "n", _get(argv, "m")), argv);
+scaffolder.magic(_get(argv, "s"), _get(argv, "n"), argv);
