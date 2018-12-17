34225c2029965fbfa1660013bd5976e1231ef860
Marcus Vinicius <maviniciuus@gmail.com>
2018-12-17 15:58:23 -0200

    [UPD] - Release 2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,31 +1,85 @@
-# Changelog
+# AF-SCAFFOLDER v2 - Changelog
 
  All relevant modifications to the `af-scaffolder` will be documented in this file following the one specified in [KEEP CHANGELOG](http://keepachangelog.com/).
 
-## 1.3.0 - 2018-12-11
-## Added
-- We added support for dynamic parameters. Any parameter that starts with -- will be passed to the template context.
+## 2.0.0 - 2018-12-17
 
-## 1.2.0 - 2018-12-11
-## Added
-- Updated the name parameter to [-n] (name), before it was [-m] (module). I added context variables to the template processor.
-
-## 1.1.2 - 2018-12-10
-## Fixed
-- Fixes in the bin entry of the package.json, to interpret the entrypoint and run as a global package.
-
-## 1.1.1 - 2018-12-10
-## Fixed
-- Correction in the "package.json" relevant to a global NPM package.
-
-## 1.1.0 - 2018-12-10
-## Added
-- Adding CHANGELOG.
-
-## 1.0.1 - 2018-12-10
 ## Changed
-- Adding the line that allows you to run the script in NPM global scope.
+- We've changed the strength of working with template arguments. Before there were the props:
+```javascript
+{{app_capitalize}}
+{{app_lower_case}}
+{{app_upper_case}}
+```
+This model was bad, for two main reasons. 1. Extreme sensitivity to maintain existing props. Affecting the template directly. 2. Whenever a new need arises a new prop needs to be implemented.
+Now the app name is available in the context of the template as: pack_name. You decide how to format:
+```javascript
+{{pack_name}}
+{{lowercase pack_name}}
+{{uppercase pack_name}}
+{{capitalize pack_name}}
+```
 
-## 1.0.0 - 2018-12-09
-## Changed
-- Publication of the functional prototype of scaffolder.
+## Added
+- With the new way of formatting and modifying the template data, we added a help kit for string and predefined date values.
+
+### String Helpers:
+- lowercase;
+- uppercase;
+- capitalize;
+- pluralize_pt;
+- singulate_pt;
+
+## Added
+- We implemented array support. Both in the Templates and in the arguments of the command line.
+
+### Command Line:
+```bash
+af-scaffolder -s ../schema-module.json -n Sample -a fields:field1,field2,field3,field4
+```
+
+Results in...
+```json
+{
+  fields: [
+    'field1',
+    'field2',
+    'field3',
+    'field4',
+  ]
+}
+```
+...at template context.
+
+#### Multiple arguments as arrays
+If you need multiple arrays at template context, dont worry about that:
+```bash
+af-scaffolder -s ../schema-module.json -n Sample -a fields:field1,field2,field3,field4 -a sidebar:menu1,menu2,menu3
+```
+Results in...
+```json
+{
+  fields: [
+    'field1',
+    'field2',
+    'field3',
+    'field4',
+  ],
+  sidebar: [
+    'menu1',
+    'menu2',
+    'menu3',
+  ]
+}
+```
+
+### Templates:
+How to use the parsed array at template:
+```javascript
+    {{#each fields}}
+      {
+        data: {{singulate_pt (lowercase this)}}, 
+        title: {{pluralize_pt (capitalize this)}}, 
+      },
+    {{/each}}
+```
--- src/scaffolder.js
+++ src/scaffolder.js
@@ -1,88 +1,99 @@
-var _get = require("lodash").get;
-var fs = require("fs");
-var handleBars = require("./helpers/handleBars");
+//dependencies
+var _get = require('lodash').get;
+var fs = require('fs');
+//src
+var handleBars = require('./handleBars');
+var files = require('./helpers/files');
+var errors = require('./helpers/errors');
 
-var stringHelper = require("./helpers/stringHelper");
-var files = require("./helpers/files");
-var errors = require("./helpers/errors");
-
-var defaultArgs = ["_", "s", "m", "n"];
-
+var defaultArgs = ['_', 's', 'm', 'n', 'a'];
 var scaffolder = {
-  boot: function(scheme_path, app, argv = {}) {
-    this.scheme_path = scheme_path;
+	boot: function(scheme_path, app, argv = {}) {
+		this.scheme_path = scheme_path;
 
-    if (files.exists(this.scheme_path)) {
-      this.pack_name = app;
-      this.models = JSON.parse(files.get_contents(this.scheme_path));
-      this.arguments = this.parse_arguments(argv);
-      return this;
-    }
+		if (files.exists(this.scheme_path)) {
+			this.pack_name = app;
+			this.models = JSON.parse(files.get_contents(this.scheme_path));
+			this.arguments = this.parse_arguments(argv);
+			return this;
+		}
 
-    throw new Error(errors.model_not_found(path));
-  },
-  parse_arguments: function(args) {
-    var auxArgs = { ...args, pack_name: this.pack_name };
+		throw new Error(errors.model_not_found(path));
+	},
+	parse_arguments: function(args) {
+		var auxArgs = { ...args, pack_name: this.pack_name };
+		// console.log('parse_arguments=> input:', args);
 
-    Object.keys(args).map(function(prop) {
-      if (defaultArgs.indexOf(prop) !== -1) {
-        delete auxArgs[prop];
-      }
-    });
+		Object.keys(args).map(function(prop) {
+			// arguments after "-a", becomes an array with af-scaffolder array parser
+			// Ex.: -a fieldName:field1,field2,field3,field4.
+			//Results: "fieldName", at template_context, value is an array like ["field1", "field2", "field3", "field4"].
+			if (prop === 'a') {
+				//input need to be an array
+				if (!Array.isArray(auxArgs[prop])) auxArgs[prop] = [auxArgs[prop]];
 
-    return auxArgs;
-  },
-  rename: function(string) {
-    return handleBars.compile_template(string, this.arguments);
-  },
-  touch_file: function(path, template = "") {
-    path = scaffolder.rename(path);
+				auxArgs[prop].map(function(param) {
+					var auxSplit = param.split(':');
+					var auxProp = auxSplit[0];
+					var auxValue = auxSplit[1].trim().split(',');
+					auxArgs[auxProp] = auxValue;
+				});
+			}
 
-    var content = "";
-    if (template) {
-      var fileTemplate = scaffolder.file_template(template);
-      content = fileTemplate;
-    }
+			// I remove reserved LIB's from the template context.
+			// This prevents templates from consuming these properties,
+			// which should change according to the evolution of LIB,
+			// without breaking their templates when updating the tool.
+			if (defaultArgs.indexOf(prop) !== -1) {
+				delete auxArgs[prop];
+			}
+		});
 
-    if (!fs.existsSync(path)) fs.writeFileSync(path, content);
-  },
-  file_template: function(template_path) {
-    template_path = handleBars.get_template_path(
-      this.scheme_path,
-      template_path
-    );
+		// console.log('parse_arguments=> result:', auxArgs);
+		return auxArgs;
+	},
+	rename: function(string) {
+		return handleBars.compile_template(string, this.arguments);
+	},
+	touch_file: function(path, template = '') {
+		path = scaffolder.rename(path);
 
-    if (files.exists(template_path)) {
-      return handleBars.compile_template(
-        files.get_contents(template_path),
-        this.arguments
-      );
-    }
+		var content = '';
+		if (template) {
+			var fileTemplate = scaffolder.file_template(template);
+			content = fileTemplate;
+		}
 
-    throw new Error(errors.template_not_found(template_path));
-  },
-  touch_folder: function(path) {
-    path = this.rename(path);
-    if (!fs.existsSync(path)) fs.mkdirSync(path);
-  },
-  build: function(models, parentPath = ".") {
-    models.map(function(model) {
-      if (_get(model, "children", []).length > 0) {
-        scaffolder.touch_folder(`${parentPath}/${model.name}`);
-        scaffolder.build(
-          model.children,
-          `${parentPath}/${scaffolder.rename(model.name)}`
-        );
-      } else {
-        scaffolder.touch_file(`${parentPath}/${model.name}`, model.template);
-      }
-    });
+		if (!fs.existsSync(path)) fs.writeFileSync(path, content);
+	},
+	file_template: function(template_path) {
+		template_path = handleBars.get_template_path(this.scheme_path, template_path);
 
-    return this;
-  },
-  magic: function(path, app, args) {
-    this.boot(path, app, args).build(this.models);
-  }
+		if (files.exists(template_path)) {
+			return handleBars.compile_template(files.get_contents(template_path), this.arguments);
+		}
+
+		throw new Error(errors.template_not_found(template_path));
+	},
+	touch_folder: function(path) {
+		path = this.rename(path);
+		if (!fs.existsSync(path)) fs.mkdirSync(path);
+	},
+	build: function(models, parentPath = '.') {
+		models.map(function(model) {
+			if (_get(model, 'children', []).length > 0) {
+				scaffolder.touch_folder(`${parentPath}/${model.name}`);
+				scaffolder.build(model.children, `${parentPath}/${scaffolder.rename(model.name)}`);
+			} else {
+				scaffolder.touch_file(`${parentPath}/${model.name}`, model.template);
+			}
+		});
+
+		return this;
+	},
+	magic: function(path, app, args) {
+		this.boot(path, app, args).build(this.models);
+	},
 };
 
 module.exports = scaffolder;
